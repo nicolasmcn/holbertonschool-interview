e21af503ab5ef5af562ff3f64c5cdcec8727f724
nicolas <nicolas@kali>
2024-08-30 18:10:14 +0200

    i forget the directory

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+def canUnlockAll(boxes):
+    n = len(boxes)
+    opened_boxes = set([0])
+    keys = boxes[0]
+    stack = keys.copy()
+
+    while stack:
+        key = stack.pop()
+
+        if key < n and key not in opened_boxes:
+            opened_boxes.add(key)
+            for new_key in boxes[key]:
+                if new_key not in opened_boxes:
+                    stack.append(new_key)
+
+    return len(opened_boxes) == n
+
