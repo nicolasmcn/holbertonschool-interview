dca6a7e426965b141cd02d33dc0435cac04a6342
nicolas <nicolas@kali>
2024-10-04 11:15:02 +0200

    remove functions

--- linked_list_palindrome/lists.h
+++ linked_list_palindrome/lists.h
@@ -18,8 +18,6 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-int cmp(listint_t *head1,listint_t *head2);
-listint_t *reverse(listint_t *head);
 
 int is_palindrome(listint_t **head);
 
