b7ed21bb6972748d44524b57ce57e1bb04ccd2e3
Mancino Nicolas <130404454+nicolasmcn@users.noreply.github.com>
2025-01-31 10:57:05 +0100

    file with all fonctions

---
+++ infinite_multiplication/holberton.h
@@ -0,0 +1,13 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stddef.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+void print_error(void);
+void _isdigit(char *str);
+int _strlen(char *str);
+int main(int argc, char *argv[]);
+
+#endif /* _HOLBERTON_H_ */
